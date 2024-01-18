d7c838c0b5e1817b0407a0d7a1bb1424c1521be7
hoho <ghdnjs18@naver.com>
2024-01-18 17:15:44 +0900

    feat: 기본 페이지 구성 추가

---
+++ src/pages/MyInfoPage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyInfoPage = () => {
+    return (
+        <div>
+            MyInfoPage
+        </div>
+    );
+};
+
+export default MyInfoPage;
---
+++ src/pages/api/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ApiPage = () => {
+    return (
+        <div>
+            ApiPage
+        </div>
+    );
+};
+
+export default ApiPage;
