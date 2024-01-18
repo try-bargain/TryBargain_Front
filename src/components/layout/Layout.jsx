d4e93d51691fc30365b11414c6c88205efda09f9
hoho <ghdnjs18@naver.com>
2024-01-18 17:16:03 +0900

    feat: 기본 컴포넌트 구성 추가

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {getFCP} from "web-vitals";
+
+const Layout = () => {
+    return (
+        <div>
+            Layout
+        </div>
+    );
+};
+
+export default Layout;
