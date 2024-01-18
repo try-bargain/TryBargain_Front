13353b7a0fdcddc3ba8c67fe54d147b13c44812d
hoho <ghdnjs18@naver.com>
2024-01-18 18:08:42 +0900

    feat: footer 기초 구현

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BsGithub } from 'react-icons/bs';
+import styles from './Footer.module.scss';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className={styles.footer}>
+            <div className='container'>
+                <div className={styles.contacts}>
+                    <a href="https://github.com">
+                        {" "}
+                        <BsGithub />
+                    </a>
+                </div>
+            </div>
+        </footer>
     );
 };
 
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const HomePage = () => {
     return (
         <div>
-            HomePage
+            HomePageHomePageHomePageHomePageHomePageHomePage
+            HomePageHomePageHomePageHomePageHomePageHomePage
+            HomePageHomePageHomePageHomePageHomePageHomePage
+            HomePageHomePageHomePageHomePageHomePageHomePage
         </div>
     );
 };
