4100a161fdbd9de3bfbce0087982a9ac3d1b7cb4
hoho <ghdnjs18@naver.com>
2024-01-18 17:35:18 +0900

    feat: 기본 화면 변경 및 의존성 추가

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import './App.css';
+import Layout from "./components/layout/Layout";
+import HomePage from "./pages/HomePage";
+import DetailPage from "./pages/DetailPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import MyInfoPage from "./pages/MyInfoPage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout/>}>
+            <Route index element={<HomePage/>}/>
+            <Route path="product/:id" element={<DetailPage/>}/>
+            <Route path="login" element={<LoginPage/>}/>
+            <Route path="register" element={<RegisterPage/>}/>
+            <Route path="myinfo" element={<MyInfoPage/>}/>
+            <Route path="*" element={<NotFoundPage/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default App;
